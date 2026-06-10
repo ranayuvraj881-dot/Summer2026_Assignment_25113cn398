1416ceb6a624a99392a37ae37b3964ad7675c804
yuvraj <ranayuvraj881@gmail.com>
2026-06-10 23:08:53 +0530

    ASSIGNMENT SOLUTION -DAY09

---
+++ .vscode/DAY-09/Q-03.c
@@ -0,0 +1,20 @@
+//write a program to print repeated character pattern
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    char ch;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        ch='A';
+        for(j=1;j<=i;j++)
+        {
+            printf("%c ",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
